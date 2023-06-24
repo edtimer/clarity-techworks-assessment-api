4b9fc979e549de45d09066e69e72761d773404a8
edtimer <b4timer@protonmail.com>
2023-06-24 04:29:30 +0100

    chore: update vite.config js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,34 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+// import { defineConfig } from 'vite';
+// import laravel from 'laravel-vite-plugin';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
-});
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: ['resources/css/app.css', 'resources/js/app.js'],
+//             refresh: true,
+//         }),
+//     ],
+// });
+const { resolve } = require('path');
+
+module.exports = {
+  alias: {
+    '@': resolve(__dirname, 'resources/js'), // Update this path to match your app's JavaScript directory
+  },
+  server: {
+    proxy: {
+      '/': {
+        target: 'http://localhost', // development url
+        changeOrigin: true,
+      },
+    },
+  },
+  build: {
+    manifest: true,
+    rollupOptions: {
+      input: {
+        app: resolve(__dirname, 'resources/js/app.js'), // path to app
+      },
+    },
+  },
+};
