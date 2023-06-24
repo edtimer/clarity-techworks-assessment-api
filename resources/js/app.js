626c5af72b4d6f2a63bed893318ef297db9abfa0
edtimer <b4timer@protonmail.com>
2023-06-24 07:36:46 +0100

    chore: move vue components from page to component folder

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,7 @@ import { createInertiaApp } from '@inertiajs/vue3'
 createInertiaApp({
   resolve: name => {
     const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
-    return pages[`./Pages/${name}.vue`]
+    return pages[`./components/${name}.vue`]
   },
   setup({ el, App, props, plugin }) {
     //the following creates a vue app
