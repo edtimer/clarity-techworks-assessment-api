d1a451e757ca39ef029e9fc249ed71c309c6aa31
edtimer <b4timer@protonmail.com>
2023-06-24 04:19:39 +0100

    chore: setup app.js with inertia

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
-import './bootstrap';
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/vue3'
+
+createInertiaApp({
+  resolve: name => {
+    const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
+    return pages[`./Pages/${name}.vue`]
+  },
+  setup({ el, App, props, plugin }) {
+    //the following creates a vue app
+    createApp({ render: () => h(App, props) })
+    //using inertia plugin
+      .use(plugin)
+      .mount(el)
+  },
+})
