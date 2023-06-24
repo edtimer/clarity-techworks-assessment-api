0cdb72b326eff51f6d3dd90aa8b7b1017d9b9053
edtimer <b4timer@protonmail.com>
2023-06-24 05:07:15 +0100

    chore: connect laravel port 8000 to inertia

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,36 @@
-// import { defineConfig } from 'vite';
-// import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue'
+export default defineConfig({
+    plugins: [
+        vue(),
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
+});
+// const { resolve } = require('path');
 
-// export default defineConfig({
-//     plugins: [
-//         laravel({
-//             input: ['resources/css/app.css', 'resources/js/app.js'],
-//             refresh: true,
-//         }),
-//     ],
-// });
-const { resolve } = require('path');
-
-module.exports = {
-  alias: {
-    '@': resolve(__dirname, 'resources/js'), // Update this path to match your app's JavaScript directory
-  },
-  server: {
-    proxy: {
-      '/': {
-        target: 'http://localhost', // development url
-        changeOrigin: true,
-      },
-    },
-  },
-  build: {
-    manifest: true,
-    rollupOptions: {
-      input: {
-        app: resolve(__dirname, 'resources/js/app.js'), // path to app
-      },
-    },
-  },
-};
+// module.exports = {
+//   alias: {
+//     '@': resolve(__dirname, 'resources/js'), // Update this path to match your app's JavaScript directory
+//   },
+//   server: {
+//     port:3000,
+//     proxy: {
+//       '/': {
+//         target: 'http://localhost:8000', // development url
+//         changeOrigin: true,
+//       },
+//     },
+//   },
+//   build: {
+//     manifest: true,
+//     rollupOptions: {
+//       input: {
+//         app: resolve(__dirname, 'resources/js/app.js'), // path to app
+//       },
+//     },
+//   },
+// };
